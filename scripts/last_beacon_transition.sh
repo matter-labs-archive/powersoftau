fc276f5ac9a48edf34e34117d295ac7fda754d60
josojo <josojo@hotmail.de>
2019-03-26 18:51:04 +0100

    finished server scripts functionality

--- scripts/last_beacon_transition.sh
+++ scripts/last_beacon_transition.sh
@@ -3,5 +3,5 @@
 ##Procedure -last setup:
 
 #Server runs the beacon step with tbd beacon input. 
-cargo run --release --bin beacon_constrained
-cargo run --release --bin verify_transform_constrained
+cargo run --release --bin beacon
+cargo run --release --bin verify_transform
